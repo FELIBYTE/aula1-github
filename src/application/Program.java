bcd9252ff64210e41c2cd7a05819be2fee3f0f28
Felipe Forischi <felipecoreio77@gmail.com>
2019-09-28 16:08:10 -0300

    Acrescentado Boa Tarde

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Eu me complico");
 		System.out.println("Bom dia");
+		System.out.println("Boa Tarde");
+
 	}
 
 }
